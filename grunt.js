777247ecb417d2e4015fd0463b76e72162d506ff
Daniele Cocca <jmc@chakra-project.org>
2012-10-29 18:52:11 +0100

    Adding a copyright banner to the minification output.

--- grunt.js
+++ grunt.js
@@ -1,9 +1,19 @@
 module.exports = function (grunt) {
 
 	grunt.initConfig({
+		meta: {
+			version: '1.0',
+			banner: '/*! MultiSet.js - v<%= meta.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'* http://www.canvace.com/\n' +
+				'* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+				'Canvace Srl */',
+		},
+
 		min: {
 			dist: {
-				src: ['src/MultiSet.js'],
+        			src: ['<banner:meta.banner>',
+				      '<file_strip_banner:src/MultiSet.js>'],
 				dest: 'bin/multiset.js',
 			},
 		},
